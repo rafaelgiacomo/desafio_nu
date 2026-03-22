a5c554bb091ab7597454bf0196b63a2d332b1fea
rafaelgiacomo <rafaelgiacomo.rrg@gmail.com>
2026-03-22 14:57:52 -0300

    Implementação 1 de divisão por usuario e merge de usuarios.

--- CloudStorageSystem.Tests/CloudStorageTests.cs
+++ CloudStorageSystem.Tests/CloudStorageTests.cs
@@ -5,39 +5,87 @@ using CloudStorage;
 public class CloudStorageTests
 {
     [Fact]
-    public void AddFile_ShouldReturnTrue_WhenFileIsNew()
+    public void AddFile_ShouldReturnTrue_WhenFileIsNewAndWithinUserCapacity()
     {
-        // Arrange (Organizar)
+        // Arrange
         var storage = new CloudStorage();
+        storage.AddUser(1, 600);
 
-        // Act (Agir)
-        bool result = storage.AddFile("foto.png", 500);
+        // Act
+        bool result = storage.AddFile(1, "foto.png", 500);
 
-        // Assert (Verificar)
+        // Assert
         Assert.True(result);
     }
 
     [Fact]
     public void AddFile_ShouldReturnFalse_WhenFileNameAlreadyExists()
     {
-        // Arrange (Organizar)
+        // Arrange
         var storage = new CloudStorage();
-       storage.AddFile("foto.png", 500);
+        storage.AddUser(1, 600);
+        storage.AddFile(1, "foto.png", 300);
 
-        // Act (Agir)
-        bool result = storage.AddFile("foto.png", 300);
+        // Act
+        bool result = storage.AddFile(1, "foto.png", 300);
 
-        // Assert (Verificar)
+        // Assert
         Assert.False(result);
+    }
+
+    [Fact]
+    public void AddFile_ShouldReturnFalse_WhenFileIsNewButAboveUserCapacity()
+    {
+        // Arrange
+        var storage = new CloudStorage();
+        storage.AddUser(1, 600);
+        storage.AddFile(1, "foto.png", 500);
+
+        // Act
+        bool result = storage.AddFile(1, "foto2.png", 300);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddFile_ShouldReturnFalse_WhenUserNotFound()
+    {
+        // Arrange
+        var storage = new CloudStorage();
+
+        // Act
+        bool result = storage.AddFile(1, "foto2.png", 300);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddFile_ShouldReturnTrue_WhenAddingSameFileForDifferentUsers()
+    {
+        // Arrange
+        var storage = new CloudStorage();
+        storage.AddUser(1, 600);
+        storage.AddUser(2, 600);
+
+        // Act
+        var result1 = storage.AddFile(1, "foto1.png", 300);
+        var result2 = storage.AddFile(2, "foto1.png", 300);
+
+        // Assert
+        Assert.True(result1);
+        Assert.True(result2);
     }
 
     [Fact]
     public void DeleteFile_ShouldReturnFile_WhenFileFound()
     {
         var storage = new CloudStorage();
-       storage.AddFile("foto.png", 500);
-        
-        var result = storage.DeleteFile("foto.png");
+        storage.AddUser(1, 600);
+        storage.AddFile(1, "foto.png", 500);
+
+        var result = storage.DeleteFile(1, "foto.png");
 
         Assert.NotNull(result);
         Assert.Equal(500, result);
@@ -47,8 +95,19 @@ public class CloudStorageTests
     public void DeleteFile_ShouldReturnNull_WhenFileNotFound()
     {
         var storage = new CloudStorage();
-        
-        var result = storage.DeleteFile("inexistente.txt");
+        storage.AddUser(1, 600);
+
+        var result = storage.DeleteFile(1, "inexistente.txt");
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void DeleteFile_ShouldReturnNull_WhenUserNotFound()
+    {
+        var storage = new CloudStorage();
+
+        var result = storage.DeleteFile(1, "inexistente.txt");
 
         Assert.Null(result);
     }
@@ -57,9 +116,10 @@ public class CloudStorageTests
     public void GetFileSize_ShouldReturnFileSize_WhenFileFound()
     {
         var storage = new CloudStorage();
-        storage.AddFile("inexistente.txt", 500);
+        storage.AddUser(1, 600);
+        storage.AddFile(1, "arquivo1.txt", 500);
 
-        var result = storage.GetFileSize("inexistente.txt");
+        var result = storage.GetFileSize(1, "arquivo1.txt");
 
         Assert.NotNull(result);
         Assert.Equal(500, result);
@@ -69,22 +129,34 @@ public class CloudStorageTests
     public void GetFileSize_ShouldReturnNull_WhenFileNotFound()
     {
         var storage = new CloudStorage();
-        var result = storage.GetFileSize("inexistente.txt");
+        storage.AddUser(1, 600);
+
+        var result = storage.GetFileSize(1, "inexistente.txt");
         Assert.Null(result);
     }
 
     [Fact]
-    public void GetNLargest_ShouldReturnLargest()
+    public void GetFileSize_ShouldReturnNull_WhenUserNotFound()
+    {
+        var storage = new CloudStorage();
+
+        var result = storage.GetFileSize(1, "arquivo1.txt");
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetNLargest_ShouldReturnLargest_OneUser()
     {
         //Arrange
         var storage = new CloudStorage();
+        storage.AddUser(1, 600);
 
-        storage.AddFile("/dir/file1.txt", 5);
-        storage.AddFile("/dir/file2", 20);
-        storage.AddFile("/dir/deeper/file2.mov", 9);
+        storage.AddFile(1, "/dir/file1.txt", 5);
+        storage.AddFile(1, "/dir/file2", 20);
+        storage.AddFile(1, "/dir/deeper/file2.mov", 9);
 
         //Act
-        List<string> result = storage.GetNLargest("/dir", 2);
+        List<string> result = storage.GetNLargest(1, "/dir", 2);
 
         //Assert
         Assert.NotEmpty(result);
@@ -94,17 +166,18 @@ public class CloudStorageTests
     }
 
     [Fact]
-    public void GetNLargest_ShouldReturnEmpty_WhenPrefixNotFound()
+    public void GetNLargest_ShouldReturnEmpty_WhenPrefixNotFound_OneUser()
     {
         //Arrange
         var storage = new CloudStorage();
+        storage.AddUser(1, 600);
 
-        storage.AddFile("/dir/file1.txt", 5);
-        storage.AddFile("/dir/file2", 20);
-        storage.AddFile("/dir/deeper/file2.mov", 9);
+        storage.AddFile(1, "/dir/file1.txt", 5);
+        storage.AddFile(1, "/dir/file2", 20);
+        storage.AddFile(1, "/dir/deeper/file2.mov", 9);
 
         //Act
-        List<string> result = storage.GetNLargest("/anotherdir", 2);
+        List<string> result = storage.GetNLargest(1, "/anotherdir", 2);
 
         //Assert
         Assert.Empty(result);
--- CloudStorageSystem/CloudStorage.cs
+++ CloudStorageSystem/CloudStorage.cs
@@ -4,32 +4,146 @@ namespace CloudStorage;
 
 public class CloudStorage
 {
+    private Dictionary<int, CloudUser> _users = new Dictionary<int, CloudUser>();
     private Dictionary<string, int> _files = new Dictionary<string, int>();
+
+    public bool AddUser(int id, int capacity)
+    {
+        if (_users.ContainsKey(id))
+            return false;
+
+        _users.Add(id, new CloudUser(id, capacity));
+
+        return true;
+    }
+
+    public bool AddFile(int userId, string name, int size)
+    {
+        if (!_users.ContainsKey(userId))
+            return false;
+
+        var user = _users.GetValueOrDefault(userId);
+
+        return user!.AddFile(name, size);
+    }
+
+    public int? DeleteFile(int userId, string name)
+    {
+        if (!_users.ContainsKey(userId))
+            return null;
+
+        var user = _users.GetValueOrDefault(userId);
+
+        return user!.DeleteFile(name);
+    }
+
+    public int? GetFileSize(int userId, string name)
+    {
+        if (!_users.ContainsKey(userId))
+            return null;
+
+        var user = _users.GetValueOrDefault(userId);
+
+        return user!.GetFileSize(name);
+    }
+
+    public List<string> GetNLargest(int userId, params string[] prefix)
+    {
+        if (!_users.ContainsKey(userId))
+            return new List<string>();
+
+        var user = _users.GetValueOrDefault(userId);
+
+        return user!.GetNLargest(prefix);
+    }
+
+    public List<string> GetNLargest(int userId, string prefix, int n)
+    {
+        if (!_users.ContainsKey(userId))
+            return new List<string>();
+
+        var user = _users.GetValueOrDefault(userId);
+
+        return user!.GetNLargest(prefix, n);
+    }
+
+    public int? MergeUsers(int userId1, int userId2)
+    {
+        var user1 = _users.GetValueOrDefault(userId1);
+        var user2 = _users.GetValueOrDefault(userId2);
+
+        if(user1 == null || user2 == null)
+            return null;
+
+        int newUserId = _users.Count + 1;
+        int newUserCapacity = user1.Capacity + user2.Capacity;
+        
+        var newUser = new CloudUser(newUserId, newUserCapacity)
+        {
+            Files = user1.MergeFiles(user2.Files, newUserId.ToString())
+        };
+
+        _users.Add(newUserId, newUser);
+
+        return newUser.Id;
+    }
+}
+
+public class CloudUser
+{
+    public int Id { get; set; }
+    public int Capacity { get; set; }
+    public Dictionary<string, int> Files { get; set; } = new Dictionary<string, int>();
+
+    public int StorageUsed => Files.Sum(x => x.Value);
+    public int StorageAvailable => Capacity - Files.Sum(x => x.Value);
+
+
+    public CloudUser(int id, int capacity)
+    {
+        Id = id;
+        Capacity = capacity;
+    }
 
     public bool AddFile(string name, int size)
     {
-        if(_files.ContainsKey(name))
+        if (Files.ContainsKey(name) || size > StorageAvailable)
             return false;
 
-        _files.Add(name, size);
+        Files.Add(name, size);
 
         return true;
     }
 
     public int? DeleteFile(string name)
     {
-        if(!_files.ContainsKey(name))
+        if (!Files.ContainsKey(name))
             return null;
 
-        var fileSize = _files.GetValueOrDefault(name);
-        _files.Remove(name);
+        var fileSize = Files.GetValueOrDefault(name);
+        Files.Remove(name);
 
         return fileSize;
     }
 
+    public Dictionary<string, int> MergeFiles(Dictionary<string, int> mergingFiles, string? diffText = "1")
+    {
+        var mergedFiles = Files.ToDictionary();
+
+        foreach(var file in mergingFiles)
+        {
+            if(!mergedFiles.ContainsKey(file.Key))
+                mergedFiles.Add(file.Key, file.Value);
+            else
+                mergedFiles.Add($"({diffText}) {file.Key}", file.Value);
+        }
+
+        return mergedFiles;
+    }
+
     public int? GetFileSize(string name)
     {
-        return _files.ContainsKey(name) ? _files.GetValueOrDefault(name) : null;
+        return Files.ContainsKey(name) ? Files.GetValueOrDefault(name) : null;
     }
 
     public List<string> GetNLargest(params string[] prefix)
@@ -44,10 +158,10 @@ public class CloudStorage
 
     private List<string> GetNLargest(string[] prefix, int? n = null)
     {
-        var query = _files
+        var query = Files
             .Where(x => prefix.Any(y => x.Key.StartsWith(y)));
 
-        if(n != null)
+        if (n != null)
             query = query.Take(n.Value);
 
         return query
@@ -57,4 +171,3 @@ public class CloudStorage
             .ToList();
     }
 }
-
